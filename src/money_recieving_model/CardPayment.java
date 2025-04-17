1ecc2530abde007487c1a2b767f6f1c95ff12941
Askar <nurbekovaskar099@gmail.com>
2025-04-18 04:19:47 +0600

    Created class for card payment type that implements Money acceptor interface

---
+++ src/money_recieving_model/CardPayment.java
@@ -0,0 +1,20 @@
+package money_recieving_model;
+
+import interfaces.MoneyAcceptor;
+
+public class CardPayment implements MoneyAcceptor {
+    @Override
+    public int getAmount() {
+        return 0;
+    }
+
+    @Override
+    public void setAmount(int amount) {
+
+    }
+
+    @Override
+    public boolean paymentRecieve(int price) {
+        return false;
+    }
+}
